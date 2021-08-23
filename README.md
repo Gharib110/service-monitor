4afb47b4a0aa2839e86265459c3f07de8fb0d26a
DapperBlondie <alirezagharib110@gmail.com>
2021-08-23 22:08:43 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Service Monitor
+ An application in Go for monitoring various services that you registered them 
+ for monitoring.
---
+++ internal/templates/templates.go
@@ -0,0 +1,17 @@
+package templates
+
+import (
+	"github.com/DapperBlondie/service-monitor/internal/models"
+)
+
+// TemplateData defines template data
+type TemplateData struct {
+	CSRFToken       string
+	IsAuthenticated bool
+	PreferenceMap   map[string]string
+	User            models.User
+	Flash           string
+	Warning         string
+	Error           string
+	GwVersion       string
+}
